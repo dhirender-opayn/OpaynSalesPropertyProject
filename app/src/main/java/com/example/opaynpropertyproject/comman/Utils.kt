0d658a485e5d0d4cc533dce45dd09a75833bf569
ASUS <dhirinder@opayn.com>
2021-10-08 18:14:10 +0530

    add wishlist property , get wishlist property delete property, and filter model and also set back press (Go Search get result )

--- app/src/main/java/com/example/opaynpropertyproject/comman/Utils.kt
+++ app/src/main/java/com/example/opaynpropertyproject/comman/Utils.kt
@@ -18,6 +18,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.Lifecycle
 import com.example.opaynpropertyproject.R
 import kotlin.reflect.KClass
 
